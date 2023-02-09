028430f155c047a88adb6af07745b1be95a8bb05
pablomartan <pablomartan@kolabnow.com>
2023-02-09 09:30:51 +0100

    feat: add styles to entry point

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+import { updateUI } from './js/updateUI'
+import { compareChordProg } from './js/app'
+
+import vari from './styles/_variables.scss'
+import base from './styles/base.scss'
+import header from './styles/header.scss'
+import main from './styles/main.scss'
+import footer from './styles/footer.scss'
+import form from './styles/form.scss'
+
+export {
+    updateUI,
+    compareChordProg
+}
+
+const input = document.getElementById('submit');
+input.addEventListener('click', compareChordProg);
