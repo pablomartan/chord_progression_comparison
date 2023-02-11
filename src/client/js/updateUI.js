226c0a31fe4cf3406bf506306ee53c68209a2743
pablomartan <pablomartan@kolabnow.com>
2023-02-11 13:24:05 +0100

    feat: show songs that use the given chord progr.
    This commit changes the way the app shows the processed info. When
    finding a matching chord progression, it creates a div with a title
    consisting of the chord progression (in functional notation) and then a
    list of songs that use it (either in full or in some section of the
    song)

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -14,24 +14,50 @@ const createSection = () => {
  * @param {Array} progs: the list of chord progressions
  */
 const updateUI = progs => {
+    console.log(progs);
     const progSect = document.getElementById('matching-chord-progs') || createSection();
     const footerCollect = document.getElementsByTagName('footer');
     const footer = footerCollect[0];
     
     if (document.getElementsByClassName('section-title').length <= 0 ) {
-        const sectTitle = document.createElement('h3');
+        const sectTitle = document.createElement('h2');
         sectTitle.innerText = 'Progressions trobades';
         sectTitle.classList.add('section-title');
         progSect.appendChild(sectTitle);
     }
         
     progs.forEach(prog => {
-        const p = document.createElement('p');
-        p.classList.add('matching-chord-progression');
-        p.innerText = prog.toString();
-        progSect.appendChild(p);
+        /*
+         * prog is a dictionary with 'prog' key (the chord
+         * progression) and a 'songs' key, a list of
+         * dictionaries with keys 'artist' and 'song', each of which is a
+         * string
+         */
+        const chordProgDiv = document.createElement('div');
+        chordProgDiv.classList.add('matching-chord-progression');
+        const newTitle = document.createElement('h3')
+        newTitle.innerText = prog['prog'];
+        const songListTitle = document.createElement('h4');
+        songListTitle.innerText = 'Algunes cançons que usen esta progressió:';
+        chordProgDiv.appendChild(newTitle);
+        chordProgDiv.appendChild(songListTitle);
+        
+        const songList = prog['songs'];
+        const listToAppend = document.createElement('ul');
+        
+        songList.forEach(songTuple => {
+            // each song is a dictionary with keys 'artist' and 'song', the
+            // values of which are strings
+            const li = document.createElement('li');
+            const artist = songTuple['artist'];
+            const song = songTuple['song'];
+            li.innerHTML = `<span class="artist">${artist}</span>, <span class="song">${song}</span>`;
+            listToAppend.appendChild(li);
+        });
+        chordProgDiv.appendChild(listToAppend);
+        progSect.appendChild(chordProgDiv);
     });
-
+    
     document.body.insertBefore(progSect, footer);        
 };
 
