5074008d286f76f8f0cdcb042a34fc941125a039
pablomartan <pablomartan@kolabnow.com>
2023-02-13 22:50:50 +0100

    feat: call function to translate from latin to am.
    See previous commit.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,8 +5,11 @@
  */
 const compareChordProg = async (e) => {
     e.preventDefault();
-    const userProg = document.getElementById('chord-input').value;
-    const body = { prog: userProg };
+    const notation = document.getElementById('notation-selector').value;
+    const userProg = document.getElementById('chord-input').value.trim();
+    let body;
+
+    notation != 'am-not' ? body = { prog: Client.translateChords(userProg) } : body = userProg;
 
     const chordProgressions = await fetch('http://localhost:8081/find', {
         method: 'POST',
@@ -16,10 +19,11 @@ const compareChordProg = async (e) => {
         },
         body: JSON.stringify(body)
     })
-    .then(result => result.json());
-
-    console.log(chordProgressions);
+    .then(result => result.json())
+    
+    Client.updateUI(JSON.parse(chordProgressions));
 };
 
-const input = document.getElementById('submit');
-input.addEventListener('click', compareChordProg);
+export {
+    compareChordProg
+}
