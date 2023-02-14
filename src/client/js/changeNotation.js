6bb6b58bd6c713df06a295265b1f7e54a3856da2
pablomartan <pablomartan@kolabnow.com>
2023-02-14 11:29:15 +0100

    feat: add change from american to latin
    This feature is actually useless in the program, but it is nice to have
    a general function that can translate between the two. Added a
    dictionary for american to latin translation by mapping the dict with
    latin to american notation

--- src/client/js/changeNotation.js
+++ src/client/js/changeNotation.js
@@ -3,7 +3,7 @@
  */
 
 // A dictionary to translate from latin notation to american notation
-const latToAm = {
+const latToAmDict = {
     'Do': 'C',
     'Re': 'D',
     'Mi': 'E',
@@ -13,24 +13,57 @@ const latToAm = {
     'Si': 'B',
 };
 
+const amToLatDict = () => {
+    const map = {}
+    for (const key in latToAm) {
+        const value = latToAm[key];
+        map[value] = key;
+    }
+    
+    return map;
+};
+
 /**
  * @description: this function translates chords from latin to american
- * notation //TODO: add translation from american to latin
- * @param {String} chords: a string with chords separated by a space
+ * notation 
+ * @param {Array} chords: an array of strings, each one is a chord
  */
-const translateChords = (chords) => {
-    const chordList = chords.split(' ');
-    const newNames = chordList.map(chord => {
+const latToAm = (chords) => {
+    const newNames = chords.map(chord => {
         if (chord.length == 2) {
-            return latToAm[chord];
+            return latToAmDict[chord];
         } else if (chord.length > 2 && chord.slice(0, 2) != 'So') {
-            return latToAm[chord.slice(0, 2)] + chord.slice(2);
+            return latToAmDict[chord.slice(0, 2)] + chord.slice(2);
         } else {
-            return latToAm[chord.slice(0, 3)] + chord.slice(3);
+            return latToAmDict[chord.slice(0, 3)] + chord.slice(3);
         }
     });
-    console.log(newNames.join(' '));
     return newNames.join(' ');
+};
+
+/**
+ * @description: translate chords from american to latin notation
+ * @param {Array} chords: an array of strings, each one is a chord
+ */
+const amToLat = chords => {
+    const newNames = chords.map(chord => {
+        return amToLatDict[chord.slice(0, 1)] + chord.slice(1);
+    });
+
+    return newNames;
+};
+
+/**
+ * @description: translate chords between latin and american notation
+ * @param {String} chords: a string with chords separated by a space
+ * @param {Number} opt: latin to american (0; default) or american to latin (1)
+ */
+const translateChords = (chords, opt) => {
+    opt = opt || 0;
+    const chordList = chords.split(' ');
+
+    const translated = opt ? amToLat(chordList) : latToAm(chordList);
+    return translated;
 };
 
 export {
