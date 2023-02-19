e61052961b9a352a7be40300024986902dd1e5c6
pablomartan <pablomartan@kolabnow.com>
2023-02-19 10:24:54 +0100

    feat: send prog and key to server for processing

--- src/client/js/chordGen.js
+++ src/client/js/chordGen.js
@@ -1,4 +1,31 @@
 /**
+ * @description: get selected progression and key, and send it to the server
+ * for processing
+ */
+const transposeProgression = () => {
+    const selector = document.getElementById('prog-selector');
+    const keyInput = document.getElementById('key');
+    const prog = selector.value;
+    const key = keyInput.value;
+    const body = {
+        'prog': prog,
+        'key': key
+    };
+
+    const transposedProgression = fetch('http://localhost:8081/gen_prog', {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+            .then(async data => await data.json())
+
+    console.log(transposedProgression);
+};
+
+/**
  * @description: populate the selector for chord progressions
  */
 const populateSelector = async () => {
@@ -17,5 +44,6 @@ const populateSelector = async () => {
 };
 
 export {
-    populateSelector
+    populateSelector,
+    transposeProgression
 }
