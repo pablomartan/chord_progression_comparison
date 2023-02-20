ae07995f2f908895bbbc9900d3bfeb88562a41d3
pablomartan <pablomartan@kolabnow.com>
2023-02-20 12:57:01 +0100

    feat: add html paths to prod webpack build

--- webpack.prod.js
+++ webpack.prod.js
@@ -36,6 +36,14 @@ module.exports = {
             template: "./src/client/html/index.html",
             filename: "./index.html"
         }),
+        new HtmlWebPackPlugin({
+            template: './src/client/html/prog_generator.html',
+            filename: 'prog_generator.html'
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/client/html/prog_analyzer.html',
+            filename: 'prog_analyzer.html'
+        }),
         new WorkboxPlugin.GenerateSW()
     ]
 }
