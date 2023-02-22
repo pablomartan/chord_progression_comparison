9d140a57d028d6d6e4f35e0bb938fdf855b8b691
pablomartan <pablomartan@kolabnow.com>
2023-02-22 22:25:20 +0100

    fix: show only one songlist at a time

--- src/client/js/showExamples.js
+++ src/client/js/showExamples.js
@@ -27,21 +27,29 @@ const showExamples = async e => {
     const prog = e.srcElement.value;
     const songs = await getExamples(prog);
     
-    const newDiv = document.createElement('div');
-    newDiv.id = 'songlist-container';
-    const newTitle = document.createElement('h2');
-    newTitle.innerText = 'Algunes cançons amb esta progressió';
-    const songList = document.createElement('ul');
-    songList.id = 'songlist';
+    if (document.getElementById('songlist-container') == undefined) {
+        const newDiv = document.createElement('div');
+        newDiv.id = 'songlist-container';
+        const newTitle = document.createElement('h2');
+        newTitle.innerText = 'Algunes cançons amb esta progressió';
+        const songList = document.createElement('ul');
+        songList.id = 'songlist';
+
+        newDiv.appendChild(newTitle);
+        newDiv.appendChild(songList);
+        document.getElementById('main-container').appendChild(newDiv);
+    }
+
+    const songList = document.getElementById('songlist');
+    while(songList.children.length > 0) {
+        songList.lastChild.remove();
+    }
+    
     songs.forEach(pair => {
         const li = document.createElement('li');
         li.innerHTML = `<span class="artist">${pair[0]}</span>, <span class="song">${pair[1]}</span>`;
         songList.appendChild(li);
     });
-
-    newDiv.appendChild(newTitle);
-    newDiv.appendChild(songList);
-    document.getElementById('main-container').appendChild(newDiv);
 };
 
 export {
